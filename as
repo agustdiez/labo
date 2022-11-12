b576ba2659360a2c2afa7f665fcdf01ffae56f23
agustdiez <agustin.diez10@gmail.com>
2022-11-12 19:06:49 -0300

    agregado

---
+++ as
@@ -0,0 +1,33 @@
+[33mf648715[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m HEAD@{0}: commit (amend): :wqAgregado de script de regresion
+[33mb483162[m HEAD@{1}: reset: moving to HEAD^
+[33m55cacd4[m HEAD@{2}: checkout: moving from main to main
+[33m55cacd4[m HEAD@{3}: commit: Agregado de archivos
+[33mb483162[m HEAD@{4}: commit: Agregado de script de regresion
+[33m83e6bad[m HEAD@{5}: commit: Armado del script de regresión logística
+[33mafff115[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m HEAD@{6}: commit: Update Experimento colectivo - Seteo de envios para semillerio
+[33m439036e[m HEAD@{7}: commit: Agregada carpeta de HT de BO 2019|2021
+[33m4bdee75[m HEAD@{8}: commit: Agregados archivos de HT de BO 2019-2021
+[33ma5ae379[m HEAD@{9}: commit: Creación de _Superados
+[33mad2d663[m HEAD@{10}: pull: Fast-forward
+[33m6f9dfdc[m HEAD@{11}: commit: Cambio TRUE en kValidation
+[33m53b35d7[m HEAD@{12}: commit: Se agrega training de 2019. No se toca validation ni testing
+[33mbc72694[m HEAD@{13}: commit: Update
+[33m62e27a1[m HEAD@{14}: commit: 5 Modelos - BO. k=1
+[33m8fda185[m HEAD@{15}: commit: Subida de BO
+[33m0fbcd04[m HEAD@{16}: commit: COrrecion Meses training
+[33m6b081cf[m HEAD@{17}: commit: BO para dataset de competencia 3
+[33m3020e02[m HEAD@{18}: commit: a
+[33m8ac0537[m HEAD@{19}: commit: Subida 1 de competencia3
+[33m9ca756b[m HEAD@{20}: pull: Fast-forward
+[33m76963f3[m HEAD@{21}: commit: Archivo excel con HT
+[33m468a840[m HEAD@{22}: pull: Merge made by the 'recursive' strategy.
+[33m67f76c3[m HEAD@{23}: commit: Subida de competencia
+[33m0137d31[m HEAD@{24}: commit: Ingreso para RStudio Cloud
+[33m5251dd5[m HEAD@{25}: pull: Fast-forward
+[33ma4bed84[m HEAD@{26}: pull: Fast-forward
+[33m89230c4[m HEAD@{27}: checkout: moving from main to main
+[33m89230c4[m HEAD@{28}: checkout: moving from main to main
+[33m89230c4[m HEAD@{29}: checkout: moving from main to main
+[33m89230c4[m HEAD@{30}: pull: Fast-forward
+[33m9ec42b1[m HEAD@{31}: pull: Fast-forward
+[33m53a90ca[m HEAD@{32}: clone: from https://github.com/agustdiez/labo.git
---
+++ src/ExperimentoColectivo/regresion_logistica.r
@@ -0,0 +1,82 @@
+#Se corre en la nube
+
+
+#Parametros del script
+PARAM <- list()
+PARAM$experimento <- "REGRESION_LOGISTICA"
+PARAM$exp_input <- "HT9420_TRAINING2019202"
+PARAM$input$dataset       <- "./datasets/competenciaFINAL_2022.csv.gz"
+PARAM$train$test <- c( 202107 )
+
+#------------------------------------------------------------------------------
+options(error = function() { 
+  traceback(20); 
+  options(error = NULL); 
+  stop("exiting after script error") 
+})
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#Aqui empieza el programa
+#Aqui se debe poner la carpeta de la computadora local
+setwd("~/buckets/b1/")   #Establezco el Working Directory
+base_dir <- "~/buckets/b1/"
+
+#creo la carpeta donde va el experimento
+dir.create( paste0( base_dir, "exp/", PARAM$experimento, "/"), showWarnings = FALSE )
+setwd(paste0( base_dir, "exp/", PARAM$experimento, "/"))   #Establezco el Working Directory DEL EXPERIMENTO
+
+
+
+#leo el nombre del expermento de la Training Strategy
+arch_TS  <- paste0( base_dir, "exp/", PARAM$exp_input, "/TrainingStrategy.txt" )
+TS  <- readLines( arch_TS, warn=FALSE )
+
+#leo el dataset donde voy a entrenar el modelo final
+arch_dataset  <- paste0( base_dir, "exp/", TS, "/dataset_train_final.csv.gz" )
+dataset  <- fread( arch_dataset )
+
+#leo el dataset donde voy a aplicar el modelo final
+arch_future  <- paste0( base_dir, "exp/", TS, "/dataset_future.csv.gz" )
+dfuture <- fread( arch_future )
+
+
+#defino la clase binaria
+dataset[ , clase01 := ifelse( clase_ternaria %in% c("BAJA+1","BAJA+2"), 1, 0 )  ]
+
+campos_buenos  <- setdiff( colnames(dataset), c( "clase_ternaria", "clase01") )
+
+
+# Modelo de regresión logistica
+
+# modelo de regresión logística 
+model_glm <- glm(data = dataset,formula = clase01 ~ ., family = 'gaussian')]
+
+
+#genero la prediccion, Scoring
+prediccion  <- predict( model_glm ,
+                        data.matrix( dfuture[ , campos_buenos, with=FALSE ] ),type="response" )
+
+tb_prediccion  <- dfuture[  , list( numero_de_cliente, foto_mes ) ]
+tb_prediccion[ , prob_glm := prediccion ]
+
+
+nom_pred  <- paste0( "prediccion_logistica",
+                      ".csv"  )
+
+fwrite( tb_prediccion,
+          file= nom_pred,
+          sep= "\t" )
+
+#Extracción del mes 202107 real
+
+#cargo el dataset de donde extraigo la clase y los IDs
+dataset  <- fread( PARAM$input$dataset )
+
+dataset_202107 <- dataset[ foto_mes %in% PARAM$train$test , ]
+dataset_202107 <- dataset_202107[,list( numero_de_cliente, clase_ternaria )]
+
+
+fwrite( dataset_202107,
+        file= "dataset_202107.csv.gz",
+        logical01= TRUE,
+        sep= "," )
